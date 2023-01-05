2be41fa35c06309b5c782401e11e349c0fb540be
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-01-05 02:02:43 -0300

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { getAllUsers } from '../service/getAllUsers'
 import { QueryClient, useQuery, dehydrate } from 'react-query'
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
@@ -19,9 +20,14 @@ function Home() {
 	const { data } = useQuery({ queryKey: 'users', queryFn: getAllUsers })
 
 	return (
-		<main>
-			<h1>NextJS App</h1>
-		</main>
+		<>
+			<Head>
+				<title>NextJS App</title>
+			</Head>
+			<main>
+				<h1>NextJS App</h1>
+			</main>
+		</>
 	)
 }
 
